736a4353415e5718ea3740d03fceb8fc952f9c33
Shofikul2024 <islamshofi942@gmail.com>
2024-11-14 23:01:29 +0600

    first commit

---
+++ basic.js
@@ -0,0 +1,37 @@
+// // console.log(5);
+// // console.log("shofikul");
+// // console.log(3.78);
+
+// //  let me="shofikul" ;
+// //  console.log(me);
+
+// // //  var me2="shofikul" ;
+
+// //  console.log(me2);
+//  me=20 ;
+//  console.log(me);
+
+//  console.log("shofikul");
+// let y= 5;
+// console.log(typeof y);
+// let x=" 5";
+// // let  x= true ;
+
+// console.log(typeof x);
+
+
+// // variable naming convension 
+//  let shofikul;
+//  let shofikul_islam;  //snake case 
+//  let shofikulIslam  //camel case 
+//  let ShofikulIslam // pascal case 
+
+ 
+ let shofikul = "my name " ;
+ console.log(shofikul);
+ let Shofikul = "your name ";
+
+ console.log(Shofikul);
+
+
+ console.log(3+2) ;
---
+++ math.js
@@ -0,0 +1,22 @@
+let x= 5;
+let y = 6;
+let z= 2.5;
+ 
+let total =x+y+z
+console.log(total);
+let sub =x-y ;
+
+console.log(sub);
+
+let add=x+y ;
+
+console.log(add);
+
+let multi =x*y ;
+console.log(multi);
+
+
+let divide=x/y ;
+console.log(divide);
+let value =divide.toFixed(4)
+console.log(value)
